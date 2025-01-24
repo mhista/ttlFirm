28de27e9375692e933794a2be001eef011806420
Diwe Innocent <diweesomchi@gmail.com>
2025-01-24 17:13:44 +0100

    error correction

--- ttlfirm/components/pages/header.jsx
+++ ttlfirm/components/pages/header.jsx
@@ -27,14 +27,14 @@ const PageHeader = ({text, text2, image, subAreas=[]}) => {
       <StickyNav />
       {/* background opacity */}
       <div
-        className={`absolute w-full z-20 bg-black opacity-40 h-[350px] sm:h-[300px] md:h-[400px] text-center top-0`}
+        className={`absolute w-full z-20 bg-black opacity-40 h-[300px] sm:h-[300px] md:h-[400px] text-center top-0`}
       ></div>
       {/* background image */}
       <img
         src={`${image ?? "/assets/images/bgg.jpg"}`}
-        className={`absolute w-full object-cover h-[350px] sm:h-[300px] md:h-[400px] z-10 top-0`}
+        className={`absolute w-full object-cover h-[300px] sm:h-[300px] md:h-[400px] z-10 top-0`}
       ></img>
-    <div className={`w-full relative h-[350px] sm:h-[300px] md:h-[400px] flex flex-col-reverse sm:flex-row-reverse items-center justify-center sm:items-center md:gap-8 pt-20 sm:pt-[10px]  sm:pl-9 md:pl-0 lg:pl-9 md:px-5`}>
+    <div className={`w-full relative h-[300px] sm:h-[300px] md:h-[400px] flex flex-col-reverse sm:flex-row-reverse items-center justify-center sm:items-center md:gap-8 pt-20 sm:pt-[10px]  sm:pl-9 md:pl-0 lg:pl-9 md:px-5`}>
         
         <div
           className={`text-white  flex flex-col gap-4 items-center  md:pl-8  w-full z-40 md:pt-20 lg:pt-14`}
